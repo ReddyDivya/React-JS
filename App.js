e9ee8cc396d4002a44f89239c2804a51030c089b
Reddy Divya <34181144+ReddyDivya@users.noreply.github.com>
2021-03-17 20:35:18 +0530

    Embedding variable in a JSX

--- App.js
+++ App.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const name = 'Manu'
+const elem = <h1>Hello {name}</h1>;
+
 class App extends Component {
   render() {
     return (
         //This is JSX
-      /*
-      Sample 1
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-      */
-       //React.createElement('div', null, 'h2', 'coding' ); //h2 is not rendered as HTML tag
-
-       React.createElement('div', null, "Outer-div",
-            React.createElement('h1', null, 'This is h1'),
-            React.createElement('h2', null, 'This is h2'),
-            React.createElement('h3', null, 'This is h3'),
-            React.createElement('h4', null, 'This is h4'),
-            React.createElement('h5', null, 'This is h5'),
-            React.createElement('h6', null, 'This is h6'),//creating h6 in a div
-            React.createElement('div', null, 'inner-div',
-              React.createElement('p', null, "inner para") 
-            )//inner div
-       )//outer div
+       React.createElement('div', null, elem)
     );
   }
 }
